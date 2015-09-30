b847be30e9fab97738d062c3f7e214791eec3aec
cvibhagool <cvibhagool@gmail.com>
2015-09-30 13:11:14 -0700

    added local authentication, signup, singin and logout

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -4,6 +4,7 @@ var db = require('../models/index');
 router.route('/')
   // Gets all users
   .get(function (req, res) {
+    db.User.create({username: 'ben', password: 'testing'});
     db.User.findAll().then(function (users) {
       res.json(users);
     });
