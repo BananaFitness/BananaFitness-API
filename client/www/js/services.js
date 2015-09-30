bf88ea625a1e76c0c556121009db02fcf6149d2a
Laura Weaver <laura.maclay.weaver@gmail.com>
2015-09-30 11:23:31 -0700

    Add user signin/signup functionality

---
+++ client/www/js/services.js
@@ -0,0 +1,42 @@
+angular.module('CovalentFitness.services', [])
+
+.factory('Auth', function ($http, $location, $window) {
+
+  var signup = function (user) {
+    return $http({
+      method: 'POST',
+      url: '/api/users/signup',
+      data: user
+    })
+    .then(function (resp) {
+      // return resp.data.token;
+    });
+  };
+
+  var login = function (user) {
+    return $http({
+      method: 'POST',
+      url: '/api/users/login',
+      data: user
+    })
+    .then(function (resp) {
+      // return resp.data.token;
+    });
+  };
+
+  var logout = function () {
+    return $http({
+      method: 'POST',
+      url: '/api/users/logout',
+    })
+    .then(function (resp) {
+      // return resp.data.token;
+    });
+  };
+
+  return {
+    signup: signup,
+    login: login,
+    logout: logout
+  };
+}
