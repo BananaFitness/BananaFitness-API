9f21a50645f4a1bd692b5600da81a2f5a4fa8ac8
Derrick J Chie <djchie.dev@gmail.com>
2015-10-01 16:58:01 -0700

    Adds CORS settings

--- server/server.js
+++ server/server.js
@@ -76,6 +76,14 @@ var app = express();
 app.use(express.static(__dirname + '/client'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  // res.header('Access-Control-Allow-Origin', 'example.com');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Configure the app to use bodyParser()
 // This will let us get the data from post
 app.use(bodyParser.urlencoded({ extended: true }));
