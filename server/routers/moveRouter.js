2e54f02fe01268e49dd005e7ac49f209148a79b5
Derrick J Chie <djchie.dev@gmail.com>
2015-10-01 18:34:32 -0700

    Implements post new moves to workout

--- server/routers/moveRouter.js
+++ server/routers/moveRouter.js
@@ -2,13 +2,15 @@ var router = require('express').Router();
 var validator = require('validator');
 var db = require('../models/index');
  
-// NEEDS TESTING
 router.route('/')
   // Creates new moves for a workout
   .post(function (req, res) {
+    if (!validator.isUUID(req.body.workoutid)) {
+      res.json('User id is not a valid UUID');
+    }
     db.Move.findOrCreate({
       where: {
-        WorkoutId: req.body.workoutid,
+        workout_id: req.body.workoutid,
         name: req.body.name,
         category: req.body.category,
         weight: req.body.weight,
@@ -16,12 +18,12 @@ router.route('/')
       }
     }).spread(function (move, created) {
       if (!created) {
-        console.log('Move already exists!');
+        console.log('Move already exists in the database!');
         // Handle sending error about move not existing
       } else {
         console.log('Move created!');
       }
-      res.json(workout);
+      res.json(move);
     });
   });
 
