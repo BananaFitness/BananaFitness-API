b058081162579bca5b2226ba10b7a6f2e925d7c2
Derrick J Chie <djchie.dev@gmail.com>
2015-10-03 21:27:22 -0700

    Added get move by move id call

--- server/routers/moveRouter.js
+++ server/routers/moveRouter.js
@@ -28,9 +28,39 @@ router.route('/')
   })
 
 router.route('/:moveid')
+  // Gets a move by moveid
+  .get(function (req, res) {
+    if (!validator.isUUID(req.params.moveid)) {
+      res.json('Move id is not a valid UUID');
+    }
+    db.Move.findOne({
+      where: {
+        id: req.params.moveid
+      }
+    }).then(function (move) {
+      if (!move) {
+        res.json('Move id does not exist in the database');
+      }
+      res.json(move);
+    });
+  })
   // Deletes move by moveid
   .delete(function (req, res) {
-
+    if (!validator.isUUID(req.body.moveid)) {
+      res.json('User id is not a valid UUID');
+    }
+    db.Move.findOne({
+      where: {
+        id: req.body.moveid
+      }
+    }).then(function (move) {
+      if (!move) {
+        res.json('Move id does not exist in the database');
+      }
+      move.destroy().then(function () {
+        console.log(args);
+      }); 
+    });
   });
 
 module.exports = router;
